35958b6ab0fd5b5e03dcd3f7cf1f0a48ba77f24c
Andrew Nguyen <nguyenandrew137@protonmail.com>
2024-09-13 20:54:21 -0400

    Finish parsing

--- src/Predicate/Parser.hs
+++ src/Predicate/Parser.hs
@@ -12,96 +12,153 @@ main =
     , func1 & lt 2 #4
     ];
 
-func1 = has 3 #1 | has 1 #2;
+func1 = {has 3 #1} | {has 1 #2};
 
-func2 = not (has 3 #3);
+func2 = ~ {has 3 #3};
  -}
 
 module Predicate.Parser (
 
 ) where
 
-import Control.Applicative (Alternative ())
-import Control.Arrow
 import Control.Monad
+import Data.Function
 import Data.Functor
-import Data.Maybe
 import qualified Data.Text as T
 import Data.Void
 import Text.Megaparsec
 import Text.Megaparsec.Char
+import Text.Parser.Combinators
 
+{- Parser -}
 type Parser = Parsec Void T.Text
 
 testParser :: Parser a -> T.Text -> Either String a
-testParser p test =
-    case parse p "source-file" test of
+testParser p test = do
+    parse p "source-file" test & \case
         Left bundle -> Left $ errorBundlePretty bundle
         Right s -> Right s
 
+{- Data Types -}
 type Identifier = String
+
 data Stmt = Assign Identifier Expr
     deriving (Show, Eq)
+
 data Expr
-    = Or Expr Expr
+    = Not Expr
+    | Or Expr Expr
     | And Expr Expr
     | Implies Expr Expr
+    | Apply Identifier [Expr]
     | Ident Identifier
+    | Card String
+    deriving (Show, Eq)
+
+data Dir2D = Backwards | Forwards
     deriving (Show, Eq)
 
 function :: Parser (String, Expr)
 function = do
-    n <- name
-    void $ space *> char '=' *> space
+    n <- identifier
+    void $ surround space $ char '='
     e <- expr
     pure (n, e)
 
--- >>> testParser name "function"
--- >>> testParser name "!function"
+-- >>> testParser identifier "function"
+-- >>> testParser identifier "!function"
 -- Right "function"
--- Left "source-file:1:1:\n  |\n1 | !function\n  | ^\nunexpected '!'\nexpecting alphanumeric character or letter\n"
-name :: Parser Identifier
-name = (maybeToList <$> optional letterChar) <> some alphaNumChar
+-- Left "source-file:1:1:\n  |\n1 | !function\n  | ^\nunexpected '!'\nexpecting letter\n"
+identifier :: Parser Identifier
+identifier = letterChar <:> many alphaNumChar
 
-chainl1 :: (Monad m, Alternative m) => m a -> m (a -> a -> a) -> m a
-chainl1 p = chainl p p
-
-chainl :: (Monad m, Alternative m) => m a -> m a -> m (a -> a -> a) -> m a
-chainl p1 p o = p1 >>= rest
-  where
-    rest l =
-        (($ l) <$> o <*> p >>= rest)
-            <|> pure l
-
-chainr1 :: (Monad m, Alternative m) => m a -> m (a -> a -> a) -> m a
-chainr1 p = chainr p p
-
-chainr :: (Monad m, Alternative m) => m a -> m a -> m (a -> a -> a) -> m a
-chainr p1 p o = p1 >>= rest
-  where
-    rest l =
-        (($ l) <$> o <*> (p >>= rest))
-            <|> pure l
+maxTerm :: Int
+maxTerm = 2
 
 -- >>> testParser expr "e1"
 -- >>> testParser expr "e1 | e2 & e3"
 -- >>> testParser expr "e1 & e2 | e3"
+-- >>> testParser expr "e1 & ( e2 | e3 )"
 -- >>> testParser expr "e1 -> e2 & e3"
 -- >>> testParser expr "e1 | e2 -> e3 & e4"
+-- >>> testParser expr "(e1 | e2) -> e3 & e4"
 -- >>> testParser expr "e1 -> e2 -> e3"
 -- Right (Ident "e1")
 -- Right (And (Or (Ident "e1") (Ident "e2")) (Ident "e3"))
 -- Right (Or (And (Ident "e1") (Ident "e2")) (Ident "e3"))
+-- Right (And (Ident "e1") (Or (Ident "e2") (Ident "e3")))
 -- Right (Implies (Ident "e1") (And (Ident "e2") (Ident "e3")))
--- Right (Or (Ident "e1") (Implies (Ident "e2") (And (Ident "e3") (Ident "e4"))))
+-- Right (Implies (Or (Ident "e1") (Ident "e2")) (And (Ident "e3") (Ident "e4")))
+-- Right (Implies (Or (Ident "e1") (Ident "e2")) (And (Ident "e3") (Ident "e4")))
 -- Right (Implies (Ident "e1") (Implies (Ident "e2") (Ident "e3")))
 expr :: Parser Expr
-expr =
-    chainr exprIdent expr exprImpl
-        `chainl1` (exprOr <|> exprAnd)
+expr = term >>= expr' maxTerm
   where
-    exprIdent = Ident <$> (space *> name <* space)
-    ex s f = string s $> f
-    exprOr = ex "|" Or
-    exprAnd = ex "&" And
-    exprImpl = ex "->" Implies
+    expr' n l =
+        ( do
+            (m, dir, o) <- choice $ binOps n
+            case dir of
+                Forwards ->
+                    o l <$> (term >>= expr' m)
+                Backwards -> do
+                    r <- term
+                    expr' m (o l r)
+        )
+            <|> if n >= maxTerm
+                then pure l
+                else expr' (n + 1) l
+
+    term =
+        (unOps >>= (<$> term))
+            <|> surround
+                space
+                ( choice
+                    [ bracket expr
+                    , bracketable exprIdent
+                    , bracketable apply
+                    ]
+                )
+
+    unOps = choice [exprNot]
+    binOps n =
+        concat . take n $
+            [ [(1,Forwards,) <$> exprImpl]
+            ,
+                [ (2,Backwards,) <$> exprOr
+                , (2,Backwards,) <$> exprAnd
+                ]
+            ]
+
+    exprNot = char '~' $> Not
+    exprIdent = bracketable $ Ident <$> surround space identifier
+    exprOr = char '|' $> Or
+    exprAnd = char '&' $> And
+    exprImpl = string "->" $> Implies
+
+bracket :: Parser a -> Parser a
+bracket p = char '(' *> p <* char ')'
+
+bracketable :: Parser a -> Parser a
+bracketable p = bracket p <|> p
+
+-- >>> testParser apply "{a #2 #1 2}"
+-- >>> testParser apply "{a ({has 2} | {give 3})}"
+-- >>> testParser apply "{a}"
+-- Right (Apply "a" [Card "2",Card "1",Ident "2"])
+-- Right (Apply "a" [Or (Apply "has" [Ident "2"]) (Apply "give" [Ident "3"])])
+-- Left "source-file:1:3:\n  |\n1 | {a}\n  |   ^\nunexpected '}'\nexpecting '#', '(', alphanumeric character, digit, or white space\n"
+apply :: Parser Expr
+apply = surround space $ do
+    void $ char '{'
+    space
+    i <- identifier
+    space
+    a <- sepBy1 arg space1
+    space
+    void $ char '}'
+    pure $ Apply i a
+
+arg :: Parser Expr
+arg = card <|> (Ident <$> some digitChar) <|> bracket expr
+  where
+    card = Card <$> (char '#' *> some digitChar)
---
+++ src/Text/Parser/Combinators.hs
@@ -0,0 +1,38 @@
+module Text.Parser.Combinators (
+    (<:>),
+    surround,
+    chainl1,
+    chainl,
+    chainr1,
+    chainr,
+) where
+
+import Control.Applicative (Alternative ())
+import Text.Megaparsec
+
+infixr 5 <:>
+(<:>) :: (Applicative f) => f a -> f [a] -> f [a]
+(<:>) = liftA2 (:)
+
+surround :: (Applicative f) => f b -> f a -> f a
+surround b p = b *> p <* b
+
+chainl1 :: (Monad m, Alternative m) => m a -> m (a -> a -> a) -> m a
+chainl1 p = chainl p p
+
+chainl :: (Monad m, Alternative m) => m a -> m a -> m (a -> a -> a) -> m a
+chainl p1 p o = p1 >>= rest
+  where
+    rest l =
+        (($ l) <$> o <*> p >>= rest)
+            <|> pure l
+
+chainr1 :: (Monad m, Alternative m) => m a -> m (a -> a -> a) -> m a
+chainr1 p = chainr p p
+
+chainr :: (Monad m, Alternative m) => m a -> m a -> m (a -> a -> a) -> m a
+chainr p1 p o = p1 >>= rest
+  where
+    rest l =
+        (($ l) <$> o <*> (p >>= rest))
+            <|> pure l
